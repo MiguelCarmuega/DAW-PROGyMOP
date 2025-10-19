90c87c706aba769eea9b69e09840d5de7351ce7c
MiguelCarmuega <miguelcarmu98@gmail.com>
2025-10-19 20:17:23 +0200

    Update Main.java

--- Proyectoinicial/src/Main.java
+++ Proyectoinicial/src/Main.java
@@ -1,4 +1,11 @@
 public class Main {
 
+// esto es un comentario
 
+// TODO Me falta esto
+
+    void main() {
+        IO.println("Este es mi primer programa en java");
+        IO.println("Esta es mi segunda línea del programa");
+    }
 }
