0363a469e53bf77fde258fcdcf20e3176c671386
MiguelCarmuega <miguelcarmu98@gmail.com>
2025-10-28 10:26:09 +0100

    update

--- Proyectoinicial/src/Main.java
+++ Proyectoinicial/src/Main.java
@@ -5,7 +5,25 @@ public class Main {
 // TODO Me falta esto
 
     public static void main(String[] args) {
-        System.out.println("Este es mi primer programa en java");
+        System.out.println("Este es mi primer programa en java\n");
         System.out.println("Esta es mi segunda línea del programa\n");
+        String miNombre = "Miguel";
+        char letraDNI = 'w';
+        int edad = 27;
+        double altura = 1.67;
+        float peso = 61.1f;
+        boolean experiencia = true;
+        System.out.println("Mi nombre es:" +miNombre);
+        System.out.println("La letra de mi DNI es: " +letraDNI);
+        System.out.println("Mi edad es: " +edad);
+        System.out.println("Mi altura es de: " +altura);
+        System.out.println("Mi peso es: " +peso);
+        System.out.println("Mi experiencia es: " +experiencia);
+
+
+
+
+
+
     }
 }
