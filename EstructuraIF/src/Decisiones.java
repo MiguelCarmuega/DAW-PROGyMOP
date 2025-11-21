2f9cbcba7c5a6ae18668d960af9a8421f503fe87
MiguelCarmuega <miguelcarmu98@gmail.com>
2025-11-21 21:36:02 +0100

    update

--- EstructuraIF/src/Decisiones.java
+++ EstructuraIF/src/Decisiones.java
@@ -1,13 +1,43 @@
 public class Decisiones {
 
-    public void estructuraIF (int nota){
+    public void estructuraIF(int nota) {
 
         System.out.println("Explicación estructura IF");
-        if (nota>=5){
+        if (nota >= 5) {
             System.out.println("El examen está aprobado");
 
         } else {
-            System.out.println("El examen está suspenso");}
+            System.out.println("El examen está suspenso");
+        }
         System.out.println("Terminando evaluación");
+    }
+
+
+    public void estructuraIFELSE(double nota) {
+
+        System.out.println("Iniciando evaluación del examen");
+        if (nota>=0&&nota<=10){
+            if (nota < 1) {
+                System.out.println("Desastroso");
+            } else if (nota<4){
+                System.out.println("Mal");
+            } else if (nota<5) {
+                System.out.println("Suspenso Raspado");
+            } else if (nota<8){
+                System.out.println("Bien");
+            } else if (nota<9) {
+                System.out.println("Notable");
+            } else if (nota<10) {
+                System.out.println("Sobresaliente");
+            } else{
+                System.out.println("Matrícula de honor");
+            }
+
+        } else {
+            System.out.println("Rango incorrecto");
+        }
+
+        System.out.println("Finalizando la evaluación");
+
     }
 }
--- EstructuraIF/src/Main.java
+++ EstructuraIF/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
     Decisiones variableDecisiones = new Decisiones();
-    variableDecisiones.estructuraIF(4);
+    //variableDecisiones.estructuraIF(4);
+        variableDecisiones.estructuraIFELSE(0.5);
     }
 }
