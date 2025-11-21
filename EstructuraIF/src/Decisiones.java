d46f31f2b795849129cac85dded1bc44a31ef58f
MiguelCarmuega <miguelcarmu98@gmail.com>
2025-11-21 18:50:57 +0100

    uodate

---
+++ EstructuraIF/src/Decisiones.java
@@ -0,0 +1,13 @@
+public class Decisiones {
+
+    public void estructuraIF (int nota){
+
+        System.out.println("Explicación estructura IF");
+        if (nota>=5){
+            System.out.println("El examen está aprobado");
+
+        } else {
+            System.out.println("El examen está suspenso");}
+        System.out.println("Terminando evaluación");
+    }
+}
--- EstructuraIF/src/Main.java
+++ EstructuraIF/src/Main.java
@@ -1,2 +1,6 @@
 public class Main {
+    public static void main(String[] args) {
+    Decisiones variableDecisiones = new Decisiones();
+    variableDecisiones.estructuraIF(4);
+    }
 }
